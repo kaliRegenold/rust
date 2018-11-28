fba50bb2c4b5a61803651fe7a27562d7aa541e65
Ceritelli <Josef.Ceritelli@mines.sdsmt.edu>
2018-11-28 16:45:05 -0700

    Parallelize Greyscale

--- photomanip/src/greyscale.rs
+++ photomanip/src/greyscale.rs
@@ -6,16 +6,17 @@
 */
 
 use image::Image;
+use rayon::prelude::*;
 
 // We need to handle grey pixels (like writing them out and what not...)
 impl Image
 {
     pub fn greyscale(&mut self) {
-        for p in &mut self.pixels {
+        self.pixels.par_iter_mut().for_each(|p|{
             p.r = 255 - p.r;
             p.g = 255 - p.g;
             p.b = 255 - p.b;
             let grey = 0.3*(p.r as f64) + 0.6*(p.g as f64) + 0.1*(p.b as f64);
-        }
+        });
     }
 }
