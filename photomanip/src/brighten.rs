64cafd9677608f8d337a63264d20f75a4530423a
Ceritelli <Josef.Ceritelli@mines.sdsmt.edu>
2018-11-28 16:44:18 -0700

    Parallize Brighten

--- photomanip/src/brighten.rs
+++ photomanip/src/brighten.rs
@@ -6,24 +6,25 @@
 */
 
 use image::Image;
+use rayon::prelude::*;
 
 impl Image
 {
     pub fn brighten(&mut self, value : i16) {
-        if(value > 0)
+        if value > 0
         {
-            for p in &mut self.pixels {
+            self.pixels.par_iter_mut().for_each(|p|{
                 p.r = p.r.saturating_add(value as u8);
                 p.g = p.g.saturating_add(value as u8);
                 p.b = p.b.saturating_add(value as u8);
-            }
+            });
         }
         else if value < 0 {
-            for p in &mut self.pixels {
+            self.pixels.par_iter_mut().for_each(|p|{
                 p.r = p.r.saturating_sub(-value as u8);
                 p.g = p.g.saturating_sub(-value as u8);
                 p.b = p.b.saturating_sub(-value as u8);
-            }
+            });
         }
     }
 }
